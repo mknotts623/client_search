6aef6653fdd79d7f9fac631a03aaee501757e8c0
mknotts623 <mknotts623@gmail.com>
2018-01-27 23:45:29 -0500

    Removed Redundancy
    Removed redundancy of id numbers for clients and tags; no longer crashes if same client or tag is entered in twice

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ def create_table():
     # need to get ALL parameters later
     c.execute('PRAGMA foreign_keys = ON;')
     c.execute('CREATE TABLE IF NOT EXISTS clients('
-              'client_id INTEGER NOT NULL, '
               'client_name TEXT PRIMARY KEY,'
-              ' email TEXT NOT NULL, '
-              'income INTEGER NOT NULL);')
+              'email TEXT NOT NULL, '
+              'income INTEGER NOT NULL)')
     c.execute('CREATE TABLE IF NOT EXISTS tags('
-              'tag_id INTEGER NOT NULL, '
-              'tag_name TEXT PRIMARY KEY);')
+              'tag_name TEXT PRIMARY KEY)')
     c.execute('CREATE TABLE IF NOT EXISTS client_tags('
               'client TEXT NOT NULL, '
               'tag TEXT NOT NULL,'
@@ -20,8 +18,24 @@ def create_table():
               'FOREIGN KEY(tag) REFERENCES tags(tag_name));')
 
 def enter_client(name, email, income):
-    #need to get ALL parameters later
-    data = c.execute("SELECT * FROM clients;")
-    id = len(data.fetchall())
-    c.execute("INSERT INTO clients(client_id, client_name, email, income) VALUES(?,?,?,?)", (id, name, email, income))
-    conn.commit()
+    #won't crash if client is already in database
+    c.execute("SELECT client_name FROM clients WHERE client_name = ?",
+              (name,))
+    exists = c.fetchone()
+    if exists:
+        print("Client already exists")
+    else:
+        c.execute("INSERT INTO clients(client_name, email, income) VALUES(?,?,?)",
+            (name, email, income))
+        conn.commit()
+
+def enter_tag (tag_name):
+    #won't crash if tag is already in database
+    c.execute("SELECT tag_name from tags WHERE tag_name = ?",
+              (tag_name,))
+    exists = c.fetchone()
+    if exists:
+        print("Tag already exists")
+    else:
+        c.execute("INSERT INTO tags(tag_name) VALUES(?)", (tag_name))
+        conn.commit()
